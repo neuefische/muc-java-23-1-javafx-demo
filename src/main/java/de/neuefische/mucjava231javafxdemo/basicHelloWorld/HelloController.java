d628951830497e022e1e49738f9906b53d3954ae
Sergej Grilborzer <sergej@grilborzer.de>
2023-06-29 11:47:06 +0200

    Create new Listener for TextField in HelloController

--- src/main/java/de/neuefische/mucjava231javafxdemo/basicHelloWorld/HelloController.java
+++ src/main/java/de/neuefische/mucjava231javafxdemo/basicHelloWorld/HelloController.java
@@ -10,6 +10,20 @@ public class HelloController {
     @FXML
     private TextField textField;
 
+    // Vorgehen:
+    // Wenn TextField eine "neue Nachricht/ Update hat" -> Update im Label reflektieren
+    // Wo füge ich das jetzt ein? -> Soll am Anfang des Objektes erstellt werden
+    // initialize wird zu Beginn der Erstellung, wenn das FXML fertig geladen hat, ausgeführt
+    public void initialize() {
+        System.out.println("Hallo i bims der HelloController!");
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Text changed from " + oldValue + " to " + newValue);
+
+            welcomeText.setText(newValue);
+        });
+    }
+
     @FXML
     void onHelloButtonClick() {
         welcomeText.setText("Hallo lieber und sehr aktiver, hellhöriger Kurs :D");
